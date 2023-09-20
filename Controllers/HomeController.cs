72f7488e3f036952c05c87e8f9fdf9a274a8975a
Vishal Prajapati <vishala.prajapati@tatvasoft.com>
2023-09-20 21:19:45 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using DotNetCore_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DotNetCore_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            //eFCodeFirstContext = new EFCodeFirstContext();
+            //efdatabaseFirstContext = new EfdatabaseFirstContext();
+        }
+
+        public IActionResult Index()
+        {
+            //eFCodeFirstContext.Employees.Add(new Employee() { });
+            //eFCodeFirstContext.SaveChanges();
+
+            //efdatabaseFirstContext.Employees.Add(new Employee() { });
+            //efdatabaseFirstContext.SaveChanges();
+
+            ViewBag.Genders = "";
+
+            ViewData["Genders"] = "";
+
+            TempData["Genders"] = "";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace DotNetCore_MVC.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "This field is required")]
+        [DisplayName("FirstName For Grid")]
+        [StringLength(50, ErrorMessage = "Max length should not be more than 50")]
+        public required string FirstName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "This field is required")]
+        [DisplayName("LastName")]
+        [StringLength(25, ErrorMessage = "Max length should not be more than 50")]
+        public required string LastName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "This field is required")]
+        [DisplayName("Gender")]
+        public int Gender { get; set; }
+
+        [DisplayName("Date of Birth")]
+        public DateTime DOB { get; set; }
+    }
+}
